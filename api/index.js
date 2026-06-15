a8fe432ee1c04b8308509c4ee8a2df80764d8701
unknown <ravishankarray480@gmail.com>
2026-06-15 17:02:35 +0530

    updated backend successfully

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 dbConnection();
 
 app.use(cors({
-  origin: ' http://localhost:5173', 
+  origin: 'https://hrms.viraladsmedia.com', 
   credentials: true,               
 }));
 app.use(express.json());
