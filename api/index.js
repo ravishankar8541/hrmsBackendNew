d4d6ca16f622f015ced4e2bc98a8443913f5e939
unknown <ravishankarray480@gmail.com>
2026-06-15 16:18:37 +0530

    first init

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 dbConnection();
 
 app.use(cors({
-  origin: 'https://hrms-frontend-fresh.vercel.app', 
+  origin: ' http://localhost:5173', 
   credentials: true,               
 }));
 app.use(express.json());
